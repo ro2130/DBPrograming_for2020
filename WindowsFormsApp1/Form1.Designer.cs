03f585d563f1f72518513b738f0fda6dd5a355b2
ro2130 <ro2130>
2020-10-08 22:23:35 +0900

    알람 수정

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(123, 69);
+            this.button1.Location = new System.Drawing.Point(311, 188);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(160, 49);
             this.button1.TabIndex = 0;
